a8254d0ae2c6422c597c26a7a03b3d456ea0dc5b
lina.wolf <lwolf@w-commerce.de>
2023-07-23 08:17:17 +0000

    [FEATURE] Introduce Intersphinx settings for Guides

---
+++ Documentation/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'My Project',
+    'version' => 'main (development)',
+    'inventories' => [
+        'h2document' => 'https://docs.typo3.org/m/typo3/docs-how-to-document/main/en-us/',
+        't3coreapi' => 'https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/',
+        't3install' => 'https://docs.typo3.org/m/typo3/guide-installation/main/en-us/',
+        't3sitepackage' => 'https://docs.typo3.org/m/typo3/tutorial-sitepackage/main/en-us/',
+        't3viewhelper' => 'https://docs.typo3.org/other/typo3/view-helper-reference/main/en-us/',
+    ],
+];
